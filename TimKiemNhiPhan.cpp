27b438710cca55403e6dfce20ab89570c9ae577e
Vutrang1310 <122619605+Vutrang1310@users.noreply.github.com>
2023-11-05 22:11:20 +0700

    Update and rename BinarySearch.cpp to TimKiemNhiPhan.cpp

---
+++ TimKiemNhiPhan.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int Binary(int a[],int n, int x){
+  int l=0,r = n - 1;
+  while (l < r){
+    int mid = (l + r) / 2;
+    if(a[mid] < x) {
+      l = mid + 1;
+    }
+    if (a[mid] > x){
+      r = mid;
+    }
+  }
+  if(a[mid] == x) {return l;}
+  return -1;
+}
+int main(){
+  int n;
+  cin >> n;
+  int a[n];
+  for(int i = 0;i< n; i++){
+    cin >> a[i];
+  }
+  cin >> x;
+  cout <<Binary(a,n,x);
+  return 0;
+}
